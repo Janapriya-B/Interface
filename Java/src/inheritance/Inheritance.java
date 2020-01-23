d26f4cbac1527d6e945058102a76b7d6f065963b
janapriya <janapriya@Janapriya-PC>
2020-01-23 13:47:12 +0530

    Inheritance initial commit

---
+++ Java/src/inheritance/Inheritance.java
@@ -0,0 +1,21 @@
+package inheritance;
+
+class Animal{  
+void eat(){System.out.println("eating...");}  
+}  
+class Dog extends Animal{  
+void bark(){System.out.println("barking...");}  
+}  
+class Cat extends Animal{  
+void meow(){System.out.println("meowing...");}  
+}  
+class TestInheritance3{  
+public static void main(String args[]){  
+Cat c=new Cat(); 
+Dog d=new Dog();
+c.meow();  
+c.eat();  
+d.eat();
+d.bark();
+//c.bark();//C.T.Error  
+}}  
