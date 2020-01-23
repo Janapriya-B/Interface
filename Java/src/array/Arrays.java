777bd9feb363de496846bcfde31599658c4af3f9
janapriya <janapriya@Janapriya-PC>
2020-01-23 16:59:14 +0530

    Arrays initial commit

---
+++ Java/src/array/Arrays.java
@@ -0,0 +1,31 @@
+package array;
+import java.util.ArrayList;
+
+ public class Arrays {
+
+	public static void main(String[] args) {
+		int[] arr = new int[4];
+		arr[0]=2;
+		arr[1]=3;
+		arr[2]=4;
+		arr[3]=5;
+		System.out.println("The thirs element is : " +arr[2]);
+		
+		ArrayList<Integer> arrL= new ArrayList<Integer>();
+		arrL.add(1);
+		arrL.add(2);
+		arrL.add(3);
+		arrL.add(4);
+		arrL.add(5);
+		System.out.println("The second element in array list is : " +arrL);
+		
+
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
